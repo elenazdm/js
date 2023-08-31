0a9177437f5a4a417d848cbe52b4d475f47232b3
Elena Shvets <133679261+elenazdm@users.noreply.github.com>
2023-08-31 13:46:41 +0200

    lessons

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// let n = -9999999999;
+// let b = NaN ** 1;
+// let c = -Infinity;
+// console.log(n, b);
+// console.log(1 / 0);
+// const t = "hello";
+// const p = "hello girls";
+// const o = `test ${2 + 2}`
+// console.log(o);
+// let yes = true;
+// let no = false;
+// let isAdmin = false;
+// let none;
+// console.log(isFinite(n));
+// console.log(typeof p);
+const a = 1;
+const b = a + 2 + "Test";
+console.log(${ a } + b + 2)
